f4dfffdcfe7658396af5182e7837b549db0da3c0
5ansus <santi02.fernandez.herrera@gmail.com>
2025-04-04 01:48:58 +0200

    Implementación de Lifecycle
    Se implementa el uso de Lifecicle en la clase GameView. Corregido un warning del build.gradle.kts

--- app/src/main/java/es/uam/eps/dadm/santioscar/renovium/GameView.kt
+++ app/src/main/java/es/uam/eps/dadm/santioscar/renovium/GameView.kt
@@ -4,24 +4,35 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import androidx.databinding.ObservableField
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import timber.log.Timber
 
-class GameView(private val context: Context) {
+class GameView(private val context: Context) : DefaultLifecycleObserver {
 
     val welcomeMessage = ObservableField<String>(context.getString(R.string.welcome_msg))
 
     fun handleStartGame() {
         Timber.d("Botón Start pulsado")
 
-        // Muestra el mensaje de inicio del juego
         Toast.makeText(
             context,
-            context.getString(R.string.game_start_msg),  // Usa el string correspondiente al idioma
+            context.getString(R.string.game_start_msg),
             Toast.LENGTH_SHORT
         ).show()
 
-        // Inicia la actividad IntroGame
         val intent = Intent(context, IntroGame::class.java)
         context.startActivity(intent)
     }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        Timber.d("Llamada a GameView.onStart")
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        Timber.d("Llamada a GameView.onStop")
+    }
 }
+
--- app/src/main/java/es/uam/eps/dadm/santioscar/renovium/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/santioscar/renovium/MainActivity.kt
@@ -10,17 +10,18 @@ import es.uam.eps.dadm.santioscar.renovium.IntroGame
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var gameView: GameView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.gameView = GameView(this) // El contexto es una instancia de Activity
+        gameView = GameView(this)
+        binding.gameView = gameView
         binding.lifecycleOwner = this
+        lifecycle.addObserver(gameView)
         Timber.tag("LOG").d("Aplicación iniciada")
-
         binding.startButton.setOnClickListener {
-            val intent = Intent(this, IntroGame::class.java)
-            startActivity(intent)
+            gameView.handleStartGame()
         }
     }
 }
